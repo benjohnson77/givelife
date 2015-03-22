18b90da9195a1d406b6afcde81f1f1a559a86048
benjohnson77 <bjohnson@ubersoftware.com>
2015-03-22 00:27:16 -0500

    got the sharing thingy working

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,29 @@
 $.noConflict();
 jQuery(document).ready(function($){
-    new Share(".share-button", {
+    
+    config = {
+      title: "Save a life in :60 seconds",
+      description: "You can change your donor status without waiting until your ID expires",   
+      image: "http://localhost:9292/media/JehPahl.png",
+      url: window.location.href,  
       networks: {
+        google_plus: {
+          enabled: true 
+        },
+        twitter: {
+          enabled: true
+        },
         facebook: {
-        app_id: "abc123"
+          enabled: true
+        },
+        pinterest: {
+          enabled: true
+        },
+        email: {
+          enabled: true
         }
+       }
       }
-    });
+
+    new Share(".share-button", config); 
 });
